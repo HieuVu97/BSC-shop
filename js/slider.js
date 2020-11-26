5542d8db7ee7cd5f389b26af1ae830dc5a73ad06
= <=>
2020-11-26 08:49:47 +0700

    update code

--- js/slider.js
+++ js/slider.js
@@ -34,13 +34,13 @@ Latest Products
  */
 
 if (slider2) {
-  new Glide("#glide_2", {
+  new Glide(glide_2, {
     type: "carousel",
     startAt: 0,
     perView: 4,
     rewin: false,
     animationDuration: 800,
-    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    animationTimingFunc: "ease-in-out",
     breakpoints: {
       1200: {
         perView: 3,
